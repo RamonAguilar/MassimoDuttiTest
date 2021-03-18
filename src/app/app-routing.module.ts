a7a39f9aa857be4d94c4f702d24772e654c45724
Ramon AR <ramonrojoaguilar@gmail.com>
2021-03-18 14:01:31 +0100

    Add auth guard
    A guard has been added that will serve to manage routes once data persistence has been implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NewLogin } from './accounts/newLogin/new-login.component.ts/new-login.component';
+import { AuthGuard } from './auth/auth.guard';
 
 // Components
+import { PrincipalComponent } from './components/principal/principal.component';
 const accountsModule = () => import('./accounts/accounts.module').then(x => x.AccountsModule);
 
 const routes: Routes = [
-  { path: '', component: NewLogin},
-  { path: 'account', loadChildren: accountsModule },
+  // { path: '', component: NewLogin},
+  { path: '', component: PrincipalComponent, canActivate: [AuthGuard] },
+  { path: 'accounts', loadChildren: accountsModule },
   { path: 'principal', loadChildren: () => import(`./components/principal/principal.module`).then(m => m.PrincipalModule) }
 ];
 
