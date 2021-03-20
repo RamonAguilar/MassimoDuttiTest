5a95ea74d07232186764b3d4181fe58ab572ebe5
Ramon AR <ramonrojoaguilar@gmail.com>
2021-03-20 12:38:05 +0100

    Debugging code

--- src/app/services/accounts.service.ts
+++ src/app/services/accounts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject, Observable, of, throwError } from 'rxjs';
@@ -16,50 +16,42 @@ export class AccountsService {
 
   constructor(
       private router: Router,
-      private http: HttpClient
   ) {
       this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('user')));
       this.user = this.userSubject.asObservable();
   }
   public get userValue(): User {
     return this.userSubject.value;
-}
-
+  }
 
   login(username, password) {
-    console.log('entra aqui username', username);
-    console.log('entra aqui password', password);
 
     const user = this.users.find(x => x.username === username && x.password === password);
 
-    console.log('entra aqui user', this.users);
-    console.log('entra es falso ??? user', user);
-
     if (!user) return this.error('Username or password is incorrect');
+    // create user in local storage to save login
     localStorage.setItem('user', JSON.stringify(user));
     this.userSubject.next(user);
     return this.ok();
   }
   
-
   logout() {
-      // remove user from local storage and set current user to null
-      localStorage.removeItem('user');
-      this.userSubject.next(null);
-      this.router.navigate(['/account/login']);
+    // remove user from local storage and set current user to null
+    localStorage.removeItem('user');
+    this.userSubject.next(null);
+    this.router.navigate(['/account/login']);
   }
 
   register(user: User) {
     
-
-      if (this.users.find(x => x.username === user.username)) {
-          return this.error('Username "' + user.username + '" is already taken')
-      }
-
-      user.id = this.users.length ? Math.max(...this.users.map(x => x.id)) + 1 : 1;
-      this.users.push(user);
-      localStorage.setItem('users', JSON.stringify(this.users));
-      return this.ok();
+    if (this.users.find(x => x.username === user.username)) {
+        return this.error('Username "' + user.username + '" is already taken')
+    }
+    // create user in local storage
+    user.id = this.users.length ? Math.max(...this.users.map(x => x.id)) + 1 : 1;
+    this.users.push(user);
+    localStorage.setItem('users', JSON.stringify(this.users));
+    return this.ok();
   
   }
 
